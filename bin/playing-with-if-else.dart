08a43ed8b6ac1e3e447d110f7a6d38274ec06c2c
Shahzaib <shahzaibnoorcs@gmail.com>
2021-04-09 17:18:54 +0500

    If else code added

---
+++ bin/playing-with-if-else.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Simple if else block.
+  if (true) {
+    print('Simple condition with default bool value true');
+  }
+  print('');
+  // Using logical characters.
+  if (4 % 2 == 0 && 5 % 2 != 0) {
+    print('4 is divisible by 2 however 5 is not.');
+  }
+  print('');
+  // If else block.
+  if (4 % 2 == 0 && 5 % 2 == 0) {
+    print('4 is divisible by 2 however 5 is not and that is right.');
+  }else {
+    print('First condition is true but second condition is false because 5 is not divisible by 2.');
+  }
+}
