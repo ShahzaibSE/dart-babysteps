f588439350fa76af0a745b5808de5599597adf03
Shahzaib <shahzaibnoorcs@gmail.com>
2021-04-25 16:34:49 +0500

    Do while loop added.

--- bin/playing-with-loops.dart
+++ bin/playing-with-loops.dart
@@ -1,6 +1,18 @@
+import "dart:io";
+//
 void main() {
   // Generating simple sequence of numbers.
   for (num i=1;i<=10;i++){
     print('Number # $i');
   }
+  //
+  // Do-while loop.
+  num do_while_count = 1;
+  print('Enter your Table number:');
+  num table_number = num.parse(stdin.readLineSync());
+  do{
+    num product = do_while_count * table_number;
+    print('$table_number x $do_while_count =  $product');
+    do_while_count++;
+  }while(do_while_count<=10);
 }
