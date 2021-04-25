e62e040df312dc401e7121cbe39a248de655d22f
Shahzaib <shahzaibnoorcs@gmail.com>
2021-04-25 22:02:29 +0500

    Loops added - more code to be added.

--- bin/playing-with-loops.dart
+++ bin/playing-with-loops.dart
@@ -1,4 +1,5 @@
-import "dart:io";
+import 'dart:io';
+import 'dart:math';
 //
 void main() {
   // Generating simple sequence of numbers.
@@ -9,10 +10,22 @@ void main() {
   // Do-while loop.
   num do_while_count = 1;
   print('Enter your Table number:');
-  num table_number = num.parse(stdin.readLineSync());
+  // num table_number = num.parse(stdin.readLineSync()); // Taking user input in the console.
+  num table_number = 2;
   do{
     num product = do_while_count * table_number;
     print('$table_number x $do_while_count =  $product');
     do_while_count++;
   }while(do_while_count<=10);
+  print('');
+  // 
+  // While loop.
+  num while_counter = 2;
+  print('Generate numbers');
+  var random = Random();
+  while(while_counter<10){
+    print(random.nextInt(50));
+    while_counter++;
+  }
+  // For loop.
 }
