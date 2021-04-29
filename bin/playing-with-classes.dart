08ccf94165527ebbfb814e69642f91432b0b109e
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-04-29 17:09:30 +0500

    Updated

--- bin/playing-with-classes.dart
+++ bin/playing-with-classes.dart
@@ -1,9 +1,30 @@
 void main() {
-  var greeter = Greeter();
-  // var greeter = new Greeter(); //Both works.
+  var greeter = Greeter('Nightbot', '2');
+  // var greeter = new Greeter('Nightbot', '2'); //Both works.
+  greeter.getGreeter();
+  print(greeter.sendMessage('Hi, how are you?'));
 }
 
 class Greeter {
   String name;
   String age;
+
+  // Long form constructor.
+  Greeter(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+  // Short form constructor.
+  // Greeter(String name, String age);
+
+  void getGreeter(){
+   print(name);
+   print(age);
+  }
+
+  String sendMessage(String message){
+    // var sender = this.name;
+    var sender = this.name;
+    return '$sender : $message';
+  }
 }
