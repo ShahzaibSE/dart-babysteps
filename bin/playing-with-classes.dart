ea601c2364bfe66862dbacea820022d22f6f3b6e
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-04-29 18:15:43 +0500

    OOP implemented.

--- bin/playing-with-classes.dart
+++ bin/playing-with-classes.dart
@@ -1,11 +1,21 @@
 void main() {
   var greeter = Greeter('Nightbot', '2');
+  var shape = Shape();
+   var list1 = ["bilal",1,true,"ali","umair"];
+  list1.replaceRange(2,4,["ahmed",false]);
   // var greeter = new Greeter('Nightbot', '2'); //Both works.
   greeter.getGreeter();
   print(greeter.sendMessage('Hi, how are you?'));
+  print(list1);
 }
 
-class Greeter {
+// Implementing abstraction.
+abstract class GreeterAbstract {
+  // We can put properties.
+  void getGreeter();
+}
+
+class Greeter extends GreeterAbstract {
   String name;
   String age;
 
@@ -17,6 +27,7 @@ class Greeter {
   // Short form constructor.
   // Greeter(String name, String age);
 
+  @override
   void getGreeter(){
    print(name);
    print(age);
@@ -26,5 +37,29 @@ class Greeter {
     // var sender = this.name;
     var sender = this.name;
     return '$sender : $message';
+  }
+}
+
+// Inheritance.
+class Shape {
+  num width;
+  num length;
+}
+
+class ShapeMetada {
+   String creator;
+   String createdAt;
+   String platform;
+}
+
+class Square extends Shape{
+  dynamic createSquare(){
+    return width * width;
+  }
+}
+
+class Rectangle extends Shape {
+  dynamic createRectangle(){
+    return width * length;
   }
 }
