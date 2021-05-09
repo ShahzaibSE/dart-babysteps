e59186f04f21e6954653e6276dd8c96058cdd2bf
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-05-10 00:57:26 +0500

    Added constants implementation

---
+++ bin/playing-with-final-and-const.dart
@@ -0,0 +1,9 @@
+void main(){
+  // There are two types of constant values, one is 'Compile-time Constant' and other one is 'Runtime Constant'.
+  final hoursSinceMidnight = DateTime.now().hour;
+  print('Hours before midnight: $hoursSinceMidnight'); 
+  // const hoursSinceMidnight_with_const = DateTime.now().hour; // We'll get error here because value is expected to be here at runtime.
+  // However, it's all good when we explicitly assign a value.
+  const num hoursSinceMidnight_with_const = 2;
+  print('Hours before midnight with const keyword: $hoursSinceMidnight_with_const');
+}
